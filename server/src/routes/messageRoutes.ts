2bc0ac9c1ee9ade204f97cbcf5a8cb83cd8234fa
Yashil Lal Singh <yashilsingh0208@gmail.com>
2025-02-17 02:00:25 +0545

    added post share and media message

--- server/src/routes/messageRoutes.ts
+++ server/src/routes/messageRoutes.ts
@@ -4,14 +4,27 @@ import {
   editMessage,
   getMessageByMessageId,
   getMessagesByChatId,
+  sendMediaMessage,
   sendMessage,
+  sharePost,
 } from "../controllers/messageController";
 import validateData from "../middlewares/validateData";
 import { sendMessageSchema } from "../lib/schemas/messageSchema";
+import upload from "../lib/multer";
+import setUploadFolder from "../middlewares/setUploadFolder";
+import cleanupUploads from "../middlewares/cleanupUploads";
 
 const router = express.Router();
 
 router.post("/chat/:chatId", validateData(sendMessageSchema), sendMessage);
+router.post(
+  "/chat/:chatId/media",
+  setUploadFolder("message"),
+  upload.array("media", 10),
+  sendMediaMessage,
+  cleanupUploads,
+);
+router.post("/chat/:chatId/post/:postId", sharePost);
 
 router.patch("/:messageId", validateData(sendMessageSchema), editMessage);
 
